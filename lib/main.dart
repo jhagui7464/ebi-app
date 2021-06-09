a1dc0023f4869a5b4943c25b396097e1e0ec7cf2
Jose Aguilar <jhagui7464@gmail.com>
2021-06-09 12:19:51 -0500

    took off debug label

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     //SystemChrome.setEnabledSystemUIOverlays([]);
     SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // is not restarted.
         primarySwatch: Colors.blue,
